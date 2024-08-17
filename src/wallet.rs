702e6072ccd76d9fa3c29b6f16deeb73e5fd9168
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-08-17 11:19:51 +0530

    saving wallet info to a file

--- src/wallet.rs
+++ src/wallet.rs
@@ -2,8 +2,39 @@ use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
+use serde::{Deserialize, Serialize};
+use std::io::BufWriter;
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
 use tiny_keccak::keccak256;
 use web3::types::Address;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(buf_writer, self)?;
+
+        Ok(())
+    }
+}
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(111);
