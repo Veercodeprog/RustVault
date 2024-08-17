550d2ffab80ee55937a9ecee98a70af8688aa6ca
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-08-17 11:08:24 +0530

    serialize based on bytes

--- src/wallet.rs
+++ src/wallet.rs
@@ -2,9 +2,18 @@ use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
-
+use tiny_keccak::Keccak256;
+use web3::types::Address;
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(111);
     secp.generate_keypair(&mut rng)
+}
+pub fn public_key_address(public_key: &PublicKey) -> Address {
+    let public_key = public_key.serialize_uncompressed();
+
+    debug_assert_eq!(public_key[0], 0x04);
+    let hash = keccak256(&public_key[1..]);
+
+    Address::from_slice(&hash[12..])
 }
