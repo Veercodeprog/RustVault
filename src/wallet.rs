767876926202bc0d00842ca6e61a69f3991a6427
veer-asbytes <veerpratap@gobazzinga.io>
2024-09-05 01:33:04 +0530

    loading crypto wallet information from file

--- src/wallet.rs
+++ src/wallet.rs
@@ -36,6 +36,22 @@ impl Wallet {
 
         Ok(())
     }
+    pub fn from_file(file_path: &str) -> Result<Wallet> {
+        let file = OpenOptions::new().read(true).open(file_path)?;
+        let buf_reader = BufReader::new(file);
+
+        let wallet: Wallet = serde_json::from_reader(buf_reader)?;
+        Ok(wallet)
+    }
+    pub fn get_secret_key(&self) -> Result<SecretKey> {
+        let secret_key = SecretKey::from_str(&self.secret_key)?;
+        Ok(secret_key)
+    }
+
+    pub fn get_public_key(&self) -> Result<PublicKey> {
+        let pub_key = PublicKey::from_str(&self.public_key)?;
+        Ok(pub_key)
+    }
 }
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
